3dcfce0c92304c3eb8d1bb97bca08df416aa696a
Mosaab Muhammed <mosaabmuhammed@outlook.com>
2020-03-09 11:21:23 +0200

    classifiers in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     version="0.0.1",
     description="Automate the process of visualization",
     py_modules=["visualizer"],
-    package_dir={'', 'src'},
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
