7217ec12a0882c064bc5b2eba3edac3c569a5cb9
Mosaab Muhammed <mosaabmuhammed@outlook.com>
2020-03-09 11:30:47 +0200

    Adding long_description & author details in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="visualizer",
     version="0.0.1",
     description="Automate the process of visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MosaabMuhammed/visualizer",
+    author="Mosaab Muhammad",
+    author_email="mosaabmuhammed@outlook.com",
     py_modules=["visualizer"],
     package_dir={'': 'src'},
     classifiers=[
